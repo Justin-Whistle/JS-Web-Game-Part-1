2b106759aa3001d2263a061d64cf7687a1db4414
Justin-Whistle <92463485+Justin-Whistle@users.noreply.github.com>
2021-12-27 18:11:18 -0600

    finished first part

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(imageSrc, imageLeft, imageBottom) {
+    let newImg = document.createElement('img')
+    newImg.src = imageSrc
+    newImg.style.position = 'fixed'
+    newImg.style.left = imageLeft
+    newImg.style.bottom = imageBottom
+    document.body.append(newImg)
+    return newImg
+}
+
+newImage('assets/green-character.gif', '100px', '100px'); //  Green character
+newImage('assets/pine-tree.png', '450px', '200px');  //  Pine tree
+newImage('assets/tree.png', '200px', '300px');  //  Tree
+newImage('assets/pillar.png', '350px', '100px');  //  Pillar
+newImage('assets/crate.png', '150px', '200px');  //  Crate
+newImage('assets/well.png', '500px', '425px');  //  Well
+
+function newItem (imageSrc, imageLeft, imageBottom) {
+    let item = newImage(imageSrc, imageLeft, imageBottom)
+
+    item.addEventListener('dblclick', function() {
+        item.remove()
+    })
+}
+
+newItem('assets/sword.png', '500px', '405px') //  Sword
+newItem('assets/sheild.png','165px', '185px')  //  Sheild
+newItem('assets/staff.png','600px', '100px')  //  Staff
